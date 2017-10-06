96a7be6696103ce80f3420e2807f68fced5c63c9
derya1 <derya@hotmail.de>
2017-10-06 09:19:20 +0200

    verbessert aufgabe1

--- aufgabe1.m
+++ aufgabe1.m
@@ -1,29 +1,41 @@
 %k soll entweder 10,50,250
-k = 10;
+
+prompt = 'please enter value for k:'
+k = input(prompt)
 
 %file definieren
 file = 'sprech.wav'
-
-%ausgabe der filegroesse
-size(file)
 
 %lesen des files in ein array
 %y = sampledata 
 %Fs = samplerate
 [y,Fs] = audioread(file);
 
-%nur positive werte des arrays (>=1)
-positive = y(y>=1);
-
-%mittelwert über k letzten werte
-avg = mean(positive((end-k):end))
+%ausgabe der filegroesse
+[samples, channels] = size (y)
 
 
-%abspielen der ungefilterten datei
-sound(y,Fs)
+%stereo -> mono
+y = (y(:,1) + y(:,2))/2;
+a=1;
+b=(1/k)*ones(1,k);
+filtered = filter(b,a,y);
 
-%abspielen der gefilterten datei
-sound(positive, Fs)
+y = y /max(abs(y));
+filtered = filtered /max(abs(filtered));
+
+subplot(2,1,2);
+plot(filtered);
+title('filtered signal')
+subplot(2,1,1);
+plot(y);
+title('unfilterted signal')
+
 
 %info inkl. samplerate
-auioinfo(file)
+sound(y,Fs)
+
+pause()
+
+%abspielen der ungefilterten datei
+sound(filtered,Fs)
