539936db790a33f21ea2b34a683fa34a5fbc38d8
derya1 <derya@hotmail.de>
2017-10-16 09:50:57 +0200

    kommentare aufgabe 1, bearbeitung aufgabe2

--- aufgabe2.m
+++ aufgabe2.m
@@ -1,29 +1,33 @@
+%laden des signal packages um chirp() benutzen zu koennen
+pkg load signal
+
 %k soll entweder 10,50,250
+
+% Chirp-Parameter
+tStart = 0; fStart = 10;
+tStop = 5; fStop = 5000;
+FSample = 1e4; TSample = 1 / FSample;
+% ----------------------------------------
+t = tStart:TSample:tStop;
+y = chirp(t, fStart, tStop, fStop, 'logarithmic');
 
 prompt = 'please enter value for k:'
 k = input(prompt)
 
-%file definieren
-file = 'sprech.wav'
-
-%lesen des files in ein array
-%y = sampledata 
-%Fs = samplerate
-[y,Fs] = audioread(file);
 
 %ausgabe der filegroesse
 [samples, channels] = size (y)
 
 
 %stereo -> mono
-y = (y(:,1) + y(:,2))/2;
+%y = (y(:,1) + y(:,2))/2;
 
 %filtern des signals
 a=1;
 b=(1/k)*ones(1,k);
 filtered = filter(b,a,y);
 
-%signalnormierung
+%signalnormierung beider signale
 y = y /max(abs(y));
 filtered = filtered /max(abs(filtered));
 
@@ -36,9 +40,9 @@ title('unfilterted signal')
 
 
 %info inkl. samplerate
-sound(y,Fs)
+sound(y,FSample)
 
 pause()
 
 %abspielen der ungefilterten datei
-sound(filtered,Fs)
+sound(filtered,FSample)
